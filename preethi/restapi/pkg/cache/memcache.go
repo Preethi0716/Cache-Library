fee131801363ec36bc42c06817ebdf24260913e4
Preethi0716 <165656302+Preethi0716@users.noreply.github.com>
2024-07-24 17:31:38 +0530

    Update memcache.go

--- preethi/restapi/pkg/cache/memcache.go
+++ preethi/restapi/pkg/cache/memcache.go
@@ -6,12 +6,10 @@ import (
 	"github.com/bradfitz/gomemcache/memcache"
 )
 
-// MemcachedCache represents a Memcached cache
 type MemcachedCache struct {
 	client *memcache.Client
 }
 
-// NewMemcachedCache creates a new MemcachedCache
 func NewMemcachedCache(address string) (*MemcachedCache, error) {
 	client := memcache.New(address)
 	if err := client.Ping(); err != nil {
@@ -20,7 +18,6 @@ func NewMemcachedCache(address string) (*MemcachedCache, error) {
 	return &MemcachedCache{client: client}, nil
 }
 
-// Set sets a value in the cache with an optional TTL
 func (c *MemcachedCache) Set(key string, value interface{}, ttl time.Duration) error {
 	item := &memcache.Item{
 		Key:        key,
@@ -30,7 +27,6 @@ func (c *MemcachedCache) Set(key string, value interface{}, ttl time.Duration) e
 	return c.client.Set(item)
 }
 
-// Get gets a value from the cache
 func (c *MemcachedCache) Get(key string) (interface{}, error) {
 	item, err := c.client.Get(key)
 	if err != nil {
@@ -39,12 +35,10 @@ func (c *MemcachedCache) Get(key string) (interface{}, error) {
 	return string(item.Value), nil
 }
 
-// Delete deletes a value from the cache
 func (c *MemcachedCache) Delete(key string) error {
 	return c.client.Delete(key)
 }
 
-// GetAll retrieves all values from the Memcached cache (not generally supported)
 func (c *MemcachedCache) GetAll() (map[string]interface{}, error) {
 	// Memcached does not support GetAll in the same way as an in-memory cache.
 	return map[string]interface{}{}, nil
