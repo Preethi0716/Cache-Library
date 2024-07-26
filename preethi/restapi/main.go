6d1a289f9376c55f1d6e0ddbba680487762b8d6b
Preethi0716 <165656302+Preethi0716@users.noreply.github.com>
2024-07-26 12:34:36 +0530

    Add files via upload

--- preethi/restapi/main.go
+++ preethi/restapi/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
-	"preethi/go/src/preethi/restapi/pkg/api"
 
+	"github.com/Preethi0716/Cache-Library/preethi/restapi/pkg/api"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	// Initialize the caches
 	unifiedCache, err := api.InitCache()
 	if err != nil {
 		log.Fatalf("Failed to initialize caches: %v", err)
@@ -16,23 +17,24 @@ func main() {
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/cache/{key}", api.HandleCacheRequest(unifiedCache)).Methods("GET", "POST", "DELETE")
+	// Register handlers
+	r.HandleFunc("/cache/{key}", api.HandleCacheRequest(unifiedCache)).Methods("GET", "DELETE", "POST")
 	r.HandleFunc("/cache", api.HandleGetAllCacheRequest(unifiedCache)).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 //Inmemory ::
-// post -- http://localhost:8080/cache/d6?cache=inMemory
+// post -- http://localhost:8080/cache/d6
 // get -- http://localhost:8080/cache/d4?cache=inMemory
 // delete -- http://localhost:8080/cache/d7?cache=inMemory
 
 // Redis ::
-// post -- http://localhost:8080/cache/d6?cache=redis
+// post -- http://localhost:8080/cache/d6
 // get -- http://localhost:8080/cache/d4?cache=redis
 // delete -- http://localhost:8080/cache/d7?cache=redis
 
 // memcached ::
-// post -- http://localhost:8080/cache/d6?cache=memcached
+// post -- http://localhost:8080/cache/d6
 // get -- http://localhost:8080/cache/d4?cache=memcached
 // delete -- http://localhost:8080/cache/d7?cache=memcached
